7f6bc52791fa613e78836aeff28da5c4e7eb15dc
chillaxt <nttjoe2020@gmail.com>
2022-12-09 15:49:36 -0800

    edited

---
+++ fancy.ps1
@@ -0,0 +1,17 @@
+#work in progress
+
+
+#define the remote target hostname (default is the local machine)
+
+
+#confirm the if the remote machine is online
+
+
+#define the target path
+
+
+#test if the file exist, True/False
+
+
+#if file exist the calc SHA256 hash and out put
+
